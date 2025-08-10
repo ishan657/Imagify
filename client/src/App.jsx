0bab276056c74cfbedbb315aaa7b32b6884fba58
ishan657 <ishansawai121@gmail.com>
2025-08-10 19:19:46 +0530

    Navbar complete on 10 aug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Home from "./pages/Home";
+import BuyCredits from "./pages/BuyCredits";
+import Result from "./pages/Result";
+import { Routes, Route } from "react-router-dom"; //establishing routes
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div></div>;
+  return (
+    //made diff routes for diff pages
+    <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/buy-credits" element={<BuyCredits />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/pages/BuyCredits.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BuyCredits = () => {
+  return <div>Buy Credits</div>;
+};
+
+export default BuyCredits;
