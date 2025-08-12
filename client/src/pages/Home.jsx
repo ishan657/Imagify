483a763a6042a8091df57154f5ecaf72b0e10b7f
ishan657 <ishansawai121@gmail.com>
2025-08-13 01:13:08 +0530

    more frontend 13aug

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,7 @@
+import Description from "../components/Description";
+import Finalbutton from "../components/Finalbutton";
 import Header from "../components/Header";
+import Reviews from "../components/Reviews";
 import Steps from "../components/Steps";
 
 const Home = () => {
@@ -6,6 +9,9 @@ const Home = () => {
     <div>
       <Header />
       <Steps />
+      <Description />
+      <Reviews />
+      <Finalbutton />
     </div>
   );
 };
